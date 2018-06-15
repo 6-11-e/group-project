2b8a572497c98d0cf98d89b62d99bad688855fec
Isaac Grey <isaac.j.grey@gmail.com>
2018-06-15 14:23:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const app = express();
 
+// PRODUCTION ONLY
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 // app middleware
@@ -12,6 +13,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
+// PRODUCTION ONLY
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
